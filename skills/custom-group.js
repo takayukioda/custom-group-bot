ee6104c03cafac2d5b78aa74cb34ac0f9f871433
takayukioda <takayuki@odashi.me>
2019-03-14 12:29:18 +0900

    wip: add group listing command

--- skills/custom-group.js
+++ skills/custom-group.js
@@ -1,21 +1,25 @@
 module.exports = (controller) => {
-  controller.hear(['groups'], 'direct_message,direct_mention,mention', () => {
+  controller.hear(['groups'], 'direct_message,direct_mention,mention', (bot, message) => {
+    bot.storage.teams.get(`${message.team}_custom_group`, (err, groups) => {
+      const groupNames = Object.keys(groups)
+      bot.reply(message, groupNames.join("\n"))
+    })
   })
-  controller.hear(['members (.*)'], 'direct_message,direct_mention,mention', () => {
+  controller.hear(['members (.*)'], 'direct_message,direct_mention,mention', (bot, message) => {
   })
-  controller.hear(['add (.*)'], 'direct_message,direct_mention,mention', () => {
+  controller.hear(['add (.*)'], 'direct_message,direct_mention,mention', (bot, message) => {
   })
-  controller.hear(['delete (.*)'], 'direct_message,direct_mention,mention', () => {
+  controller.hear(['delete (.*)'], 'direct_message,direct_mention,mention', (bot, message) => {
   })
-  controller.hear(['join (.*)'], 'direct_message,direct_mention,mention', () => {
+  controller.hear(['join (.*)'], 'direct_message,direct_mention,mention', (bot, message) => {
   })
-  controller.hear(['leave (.*)'], 'direct_message,direct_mention,mention', () => {
+  controller.hear(['leave (.*)'], 'direct_message,direct_mention,mention', (bot, message) => {
   })
-  controller.hear(['invite (.*) to (.*)'], 'direct_message,direct_mention,mention', () => {
+  controller.hear(['invite (.*) to (.*)'], 'direct_message,direct_mention,mention', (bot, message) => {
   })
-  controller.hear(['remove (.*) from (.*)'], 'direct_message,direct_mention,mention', () => {
+  controller.hear(['remove (.*) from (.*)'], 'direct_message,direct_mention,mention', (bot, message) => {
   })
-  controller.hear(['g:(.*)'], 'direct_message,direct_mention,mention', () => {
+  controller.hear([' ?g:(.*) ?'], 'direct_message,direct_mention,mention', (bot, message) => {
   })
 }
 
