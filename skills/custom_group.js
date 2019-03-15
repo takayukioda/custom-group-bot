d1a56230d9d41858ad29a2276498b2d6ae5c94ba
takayukioda <takayuki@odashi.me>
2019-03-15 15:21:53 +0900

    style: format by linter

--- skills/custom_group.js
+++ skills/custom_group.js
@@ -1,41 +1,33 @@
 module.exports = (controller) => {
-  controller.hear(['groups'], 'direct_message,direct_mention,mention', (bot, message) => {
+  controller.hear(["groups"], "direct_message,direct_mention,mention", (bot, message) => {
     bot.storage.teams.get(`${message.team}_custom_groups`, (err, groups = {}) => {
-      const groupNames = Object.keys(groups)
-      bot.reply(message, groupNames.join("\n"))
-    })
-  })
-  controller.hear(['members (.*)'], 'direct_message,direct_mention,mention', (bot, message) => {
-    const groupName = message.match[1]
+      const groupNames = Object.keys(groups);
+      bot.reply(message, groupNames.join("\n"));
+    });
+  });
+  controller.hear(["members (.*)"], "direct_message,direct_mention,mention", (bot, message) => {
+    const groupName = message.match[1];
     bot.storage.teams.get(`${message.team}_custom_groups`, (err, groups = {}) => {
-      const members = groups[groupName]
+      const members = groups[groupName];
       if (members === undefined) {
-        bot.reply(message, `There was no group named ${groupName}.`)
+        bot.reply(message, `There was no group named ${groupName}.`);
       } else if (members.length === 0) {
-        bot.reply(message, `You have no members in your group`)
+        bot.reply(message, "You have no members in your group");
       }
-      const members = groups[groupName] || []
-      let message
-      if (members.length === 0) {
-        message = ''
-      }
-
-      bot.reply(message, groupNames.join("\n"))
-    })
-  })
-  controller.hear(['add (.*)'], 'direct_message,direct_mention,mention', (bot, message) => {
-  })
-  controller.hear(['delete (.*)'], 'direct_message,direct_mention,mention', (bot, message) => {
-  })
-  controller.hear(['join (.*)'], 'direct_message,direct_mention,mention', (bot, message) => {
-  })
-  controller.hear(['leave (.*)'], 'direct_message,direct_mention,mention', (bot, message) => {
-  })
-  controller.hear(['invite (.*) to (.*)'], 'direct_message,direct_mention,mention', (bot, message) => {
-  })
-  controller.hear(['remove (.*) from (.*)'], 'direct_message,direct_mention,mention', (bot, message) => {
-  })
-  controller.hear([' ?g:(.*) ?'], 'direct_message,direct_mention,mention', (bot, message) => {
-  })
-}
-
+    });
+  });
+  controller.hear(["add (.*)"], "direct_message,direct_mention,mention", (bot, message) => {
+  });
+  controller.hear(["delete (.*)"], "direct_message,direct_mention,mention", (bot, message) => {
+  });
+  controller.hear(["join (.*)"], "direct_message,direct_mention,mention", (bot, message) => {
+  });
+  controller.hear(["leave (.*)"], "direct_message,direct_mention,mention", (bot, message) => {
+  });
+  controller.hear(["invite (.*) to (.*)"], "direct_message,direct_mention,mention", (bot, message) => {
+  });
+  controller.hear(["remove (.*) from (.*)"], "direct_message,direct_mention,mention", (bot, message) => {
+  });
+  controller.hear([" ?g:(.*) ?"], "direct_message,direct_mention,mention", (bot, message) => {
+  });
+};
